0fe6732a9e49048fa06a696c40dc9b0f21fcd1ed
Mackenzie F <fearkenz@gmail.com>
2020-10-07 17:06:51 -0700

    BIT371 app1, changed function of button, added second activity that does nothing yet.

---
+++ app/src/main/java/com/example/a375app1/Activity2.java
@@ -0,0 +1,26 @@
+package com.example.a375app1;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        TextView tv2 = findViewById(R.id.txt2);
+        String str2 = getIntent().getStringExtra("msg");
+        
+
+            }
+        }
