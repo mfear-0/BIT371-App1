e71e8c463d81506042b72c0dc872197b176955f0
Mackenzie F <fearkenz@gmail.com>
2020-10-12 14:50:44 -0700

    BIT371 app1, changed functionality to mimic a login page. compares username and password against values in second activity, returns successful or unsuccessful log in attempt.

--- app/src/main/java/com/example/a375app1/Activity2.java
+++ app/src/main/java/com/example/a375app1/Activity2.java
@@ -8,19 +8,30 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+    private String username = "bob";
+    private String password = "pass";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+        String userac = getIntent().getStringExtra("Username:");
+        String passac = getIntent().getStringExtra("Password:");
         TextView tv2 = findViewById(R.id.txt2);
-        String str2 = getIntent().getStringExtra("msg");
-        
 
-            }
+        if(username.equals(userac) && password.equals(passac)){
+            tv2.setText("Logged in as " + userac);
+            Log.i("INFO", "Log in successful, attempted log in with username: "+userac+" and password: "+passac+ " matched.");
         }
+        else{
+            tv2.setText("Username or Password entered incorrectly");
+            Log.i("INFO", "Log in unsuccessful, attempted log in with username: "+userac+" and password: "+passac+ "  do not match saved username: "+username+" and password: "+password+".");
+        }
+    }
+}
